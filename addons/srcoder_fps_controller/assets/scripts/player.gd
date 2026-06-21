f571038696fc55c60612c9f9e22bfae04e5889c8
SteveRodkiss <SteveRodkiss@users.noreply.github.com>
2026-06-22 09:48:29 +1200

    Change input direction from forward/back to up/down

--- addons/srcoder_fps_controller/assets/scripts/player.gd
+++ addons/srcoder_fps_controller/assets/scripts/player.gd
@@ -32,7 +32,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "forward", "back")
+	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y))
 	var target_velocity := Vector3.ZERO
 	if direction:
